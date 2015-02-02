cc338a5cc74187d59e2c1e4d2dfe82452b9a98ee
Park <jungu10@hanmail.net>
2015-02-02 14:43:01 +0900

    this file

---
+++ TheONe/TheONe/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TheONe
+//
+//  Created by The One Mini1 on 2015. 2. 2..
+//  Copyright (c) 2015년 Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var Date: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TheONe/TheOneView.swift
@@ -0,0 +1,11 @@
+//
+//  TheOneView.swift
+//  TheONe
+//
+//  Created by The One Mini1 on 2015. 2. 2..
+//  Copyright (c) 2015년 Park. All rights reserved.
+//
+
+import Foundation
+
+
